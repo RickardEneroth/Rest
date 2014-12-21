d0c404f0d52d263954d00896c9e657c056df8c36
Rickard <rickard_eneroth@hotmail.com>
2014-12-21 21:14:19 +0100

    Gson

--- src/main/java/se/eneroth/MyResourceJson.java
+++ src/main/java/se/eneroth/MyResourceJson.java
@@ -3,14 +3,24 @@ package se.eneroth;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 @Path("json")
 public class MyResourceJson {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public String getIt() {
-        return "JSON: Hello Jersey!";
+    public String getIt(@QueryParam("name") String name) {
+        Gson gson = new GsonBuilder().create();
+        JsonGreeeting greeting = new JsonGreeeting();
+        greeting.setGreeting("Hello Json world!!");
+        greeting.setVersion("1.0");
+        greeting.setName(name);
+        String jsonString = gson.toJson(greeting);
+
+        return jsonString;
     }
 }
