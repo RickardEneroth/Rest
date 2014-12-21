802e21a073837cc3b3fde34bd22b805028a94228
Rickard <rickard_eneroth@hotmail.com>
2014-12-21 21:59:02 +0100

    Gson

---
+++ src/main/java/se/eneroth/JsonPost.java
@@ -0,0 +1,23 @@
+package se.eneroth;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+@Path("jsonpost")
+public class JsonPost {
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getIt(JsonGreeeting greeting) {
+        JsonGreeeting reply = new JsonGreeeting();
+        reply.setName(greeting.getName());
+        reply.setGreeting("And this is the reply....");
+        Gson gson = new GsonBuilder().create();
+        String jsonString = gson.toJson(reply);
+
+        return jsonString;
+    }
+}
