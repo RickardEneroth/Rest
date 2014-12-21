05b5c1e562206a367b194460fce6df94667fc047
Rickard <rickard_eneroth@hotmail.com>
2014-12-21 21:15:49 +0100

    Gson

---
+++ src/main/java/se/eneroth/JsonGreeeting.java
@@ -0,0 +1,34 @@
+package se.eneroth;
+
+/**
+ * Created by Rickard on 2014-12-21.
+ */
+public class JsonGreeeting {
+    private String greeting;
+    private String version;
+    private String name;
+
+    public String getGreeting() {
+        return greeting;
+    }
+
+    public void setGreeting(String greeting) {
+        this.greeting = greeting;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
